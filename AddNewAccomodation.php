0026777845c6c7556a4f56ffe063f204a107ec69
Ahmed-I <a_huss04@uni-muenster.de>
2014-01-16 16:45:13 +0100

    AddNewAccomodation

---
+++ AddNewAccomodation.php
@@ -0,0 +1,14 @@
+<html> 
+    <body margin= "0"> 
+
+<font color="#0080C0"><label>The Coordinates of the New Accommodation is: </label></font>
+<br>
+
+      <?php 
+
+			$NewAccomodationCoord = $_GET['Coord'];; 
+			echo ($NewAccomodationCoord);
+			
+        ?> 
+     
+</html>
